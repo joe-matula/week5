a019bf2aca5e882be15ef2c6f4a6603eb329935a
Joe Matula <joe.matula@gmail.com>
2017-02-23 02:39:37 +0000

    added store page and carts

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,18 @@
 Rails.application.routes.draw do
+  resources :line_items
+  resources :carts
+  resources :cart
+  get 'store/index'
+
   get 'static_pages/home'
 
   get 'static_pages/about'
 
   get 'static_pages/links'
   
+  resources :store
   resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  root 'products#index'
+  root 'store#index'
 end
